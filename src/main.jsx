676f7ded5873a8a61db3de2e6f050d368d51d619
harshit <abcom@harshits-MacBook-Air.local>
2026-06-16 22:03:37 +0530

    feat: add How it Works page and update header links layout

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import Addresses from './pages/Addresses.jsx';
 import Lots from './pages/Lots.jsx';
 import PaymentPage from './pages/PaymentPage.jsx';
 import AdminPage from './pages/AdminPage.jsx';
+import HowItWorks from './pages/HowItWorks.jsx';
 import './index.css';
 
 const GOOGLE_CLIENT_ID = "933530419477-j18kgi9u914anhe3j8jf1chgfrva2rkr.apps.googleusercontent.com";
@@ -33,6 +34,7 @@ createRoot(document.getElementById('root')).render(
               <Route path="/addresses" element={<Addresses />} />
               <Route path="/pay" element={<PaymentPage />} />
               <Route path="/admin" element={<AdminPage />} />
+              <Route path="/how-it-works" element={<HowItWorks />} />
             </Routes>
           </AuthProvider>
         </BrowserRouter>
